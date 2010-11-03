74e5534f70bcdf656121e38c46b915ff856569de
Emmanuel Hugonnet <emmanuel.hugonnet@gmail.com>
2010-11-03 16:13:29 +0100

    Installer working on macosx and linux

--- src/main/resources/package/silverpeas-5.4-SNAPSHOT/bin/start_db.sh
+++ src/main/resources/package/silverpeas-5.4-SNAPSHOT/bin/start_db.sh
@@ -4,4 +4,4 @@
 export JAVA_HOME=${JAVA_HOME}
 export CLASSPATH=${INSTALL_PATH}/bin/jar/h2.jar
 
-exec $JAVA_HOME/bin/java -classpath $CLASSPATH org.h2.tools.Server -tcp -baseDir $SILVERPEAS_HOME/h2 -trace
+exec $JAVA_HOME/bin/java -classpath $CLASSPATH org.h2.tools.Server -tcp -baseDir ${INSTALL_PATH}/h2 -trace
