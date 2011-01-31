ae478a0617d698f4c5b85c6b9469266150f6881c
Emmanuel Hugonnet <emmanuel.hugonnet@gmail.com>
2011-01-31 11:01:58 +0100

    Updating to 5.5

--- src/main/resources/package/silverpeas-5.5/bin/dbBuilder.sh
+++ src/main/resources/package/silverpeas-5.5/bin/dbBuilder.sh
@@ -16,7 +16,7 @@ SILVERPEAS_PROPERTIES=$SILVERPEAS_HOME/properties
 
 LINEARGS="-T $SRV_SERVERTYPE $ACTIONPARAM $VERBOSEPARAM"
 
-CLASSPATH=$SILVERPEAS_HOME/bin/jar/application-builder.jar:$SILVERPEAS_HOME/bin/jar/dbbuilder.jar:$SILVERPEAS_HOME/bin/jar/fileutil.jar:$SILVERPEAS_HOME/bin/jar/silverpeas-settings.jar:$SILVERPEAS_HOME/bin/jar/commons-dbcp.jar:$SILVERPEAS_HOME/bin/jar/commons-io.jar:$SILVERPEAS_HOME/bin/jar/commons-logging.jar:$SILVERPEAS_HOME/bin/jar/commons-pool.jar:$SILVERPEAS_HOME/bin/jar/activation.jar:$SILVERPEAS_HOME/bin/jar/mail.jar:$SILVERPEAS_HOME/bin/jar/log4j.jar:$SILVERPEAS_HOME/bin/jar/jtds.jar:$SILVERPEAS_HOME/bin/jar/bsh.jar:$SILVERPEAS_HOME/bin/jar/jdom.jar:$SILVERPEAS_HOME/bin/jar/jcl-over-slf4j.jar:$SILVERPEAS_HOME/bin/jar/slf4j-api.jar:$SILVERPEAS_HOME/bin/jar/slf4j-log4j12.jar:$SILVERPEAS_HOME/bin/jar/spring.jar:$SILVERPEAS_HOME/bin/jar/postgresql.jar:$SILVERPEAS_HOME/bin/jar/h2-1.2.143.jar
+CLASSPATH=$SILVERPEAS_HOME/bin/jar/application-builder.jar:$SILVERPEAS_HOME/bin/jar/dbbuilder.jar:$SILVERPEAS_HOME/bin/jar/fileutil.jar:$SILVERPEAS_HOME/bin/jar/silverpeas-settings.jar:$SILVERPEAS_HOME/bin/jar/commons-dbcp.jar:$SILVERPEAS_HOME/bin/jar/commons-io.jar:$SILVERPEAS_HOME/bin/jar/commons-logging.jar:$SILVERPEAS_HOME/bin/jar/commons-pool.jar:$SILVERPEAS_HOME/bin/jar/activation.jar:$SILVERPEAS_HOME/bin/jar/mail.jar:$SILVERPEAS_HOME/bin/jar/log4j.jar:$SILVERPEAS_HOME/bin/jar/jtds.jar:$SILVERPEAS_HOME/bin/jar/bsh.jar:$SILVERPEAS_HOME/bin/jar/jdom.jar:$SILVERPEAS_HOME/bin/jar/jcl-over-slf4j.jar:$SILVERPEAS_HOME/bin/jar/slf4j-api.jar:$SILVERPEAS_HOME/bin/jar/slf4j-log4j12.jar:$SILVERPEAS_HOME/bin/jar/spring.jar:$SILVERPEAS_HOME/bin/jar/postgresql.jar:$SILVERPEAS_HOME/bin/jar/h2.jar
 export CLASSPATH
 
 echo $CLASSPATH
