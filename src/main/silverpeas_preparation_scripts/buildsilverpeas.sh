452ea60a4c105f0ce84dac816eacb548c90ac2c6
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2014-03-14 20:44:01 +0100

    Refactor the way the izpack installer for Silverpeas is built. Now, the XML configuration files, OS-specific, are directly downloaded from GitHub. The Silverpeas is also downloaded from www.silverpeas.org and customized by the settings defined in this project

--- src/main/silverpeas_preparation_scripts/buildsilverpeas.sh
+++ src/main/silverpeas_preparation_scripts/buildsilverpeas.sh
@@ -10,36 +10,54 @@ elif [ $1 = "-h" ] || [ $1 = "--help" ]; then
 fi
 
 SILVERPEAS_VERSION="$1"
-echo "Prepare Silverpeas $1"
+echo "Prepare Silverpeas $1 for packaging"
 
-MAIN_VERSION=$SILVERPEAS_VERSION
-MINOR_VERSION=`echo $SILVERPEAS_VERSION | cut -d '.' -f 3`
-test "$MINOR_VERSION" != "" && MAIN_VERSION=${SILVERPEAS_VERSION%??}
+MAIN_VERSION=${SILVERPEAS_VERSION}
+MINOR_VERSION=`echo ${SILVERPEAS_VERSION} | cut -d '.' -f 3`
+test "${MINOR_VERSION}" != "" && MAIN_VERSION=${SILVERPEAS_VERSION%??}
 
 cd `dirname $0`
-export SCRIPTS_HOME=$PWD
-echo SCRIPTS_HOME = $SCRIPTS_HOME
-export SILVERPEAS_HOME=$SCRIPTS_HOME/../resources/package/silverpeas-$MAIN_VERSION
+SCRIPTS_HOME=$PWD
+SETTINGS="${SCRIPTS_HOME}/../resources/package/settings"
+UNIX_SETTINGS="${SETTINGS}/unix"
+WINDOWS_SETTINGS="${SETTINGS}/windows"
+export SILVERPEAS_HOME="${SCRIPTS_HOME}/../resources/package/silverpeas-${MAIN_VERSION}"
 
 offline="-o"
 test $# -gt 0 && offline=""
 
-echo "Get Silverpeas ${SILVERPEAS_VERSION}"
-wget http://www.silverpeas.org/files/silverpeas-${SILVERPEAS_VERSION}-jboss6.tar.gz
+echo "  -> get Silverpeas ${SILVERPEAS_VERSION} for both Unix and Windows..."
+rm -rf ${SILVERPEAS_HOME}/../silverpeas-*
+rm ${SILVERPEAS_HOME}/../h2/*
+pushd ${UNIX_SETTINGS}
+rm *
+wget -c https://github.com/Silverpeas/Silverpeas-installer-package/raw/installation-${SILVERPEAS_VERSION}/src/main/config/settings/jboss6/linux/JBossSettings.xml
+wget -c https://github.com/Silverpeas/Silverpeas-installer-package/raw/installation-${SILVERPEAS_VERSION}/src/main/config/settings/jboss6/linux/SilverpeasSettings.xml
+popd
+pushd ${WINDOWS_SETTINGS}
+rm *
+wget -c https://github.com/Silverpeas/Silverpeas-installer-package/raw/installation-${SILVERPEAS_VERSION}/src/main/config/settings/jboss6/windows/JBossSettings.xml
+wget -c https://github.com/Silverpeas/Silverpeas-installer-package/raw/installation-${SILVERPEAS_VERSION}/src/main/config/settings/jboss6/windows/SilverpeasSettings.xml
+popd
+wget -c http://www.silverpeas.org/files/silverpeas-${SILVERPEAS_VERSION}-jboss6.tar.gz
 test $? -eq 0 && tar zxvf silverpeas-${SILVERPEAS_VERSION}-jboss6.tar.gz
-test $? -eq 0 && cp -r silverpeas-${SILVERPEAS_VERSION}*-jboss6/bin/pom.xml silverpeas-${SILVERPEAS_VERSION}*-jboss6/bin/jar silverpeas-${SILVERPEAS_VERSION}*-jboss6/bin/scripts $SILVERPEAS_HOME/bin
-rm -rf $SILVERPEAS_HOME/setup/appserver/jboss6
-cp -r silverpeas-${SILVERPEAS_VERSION}*-jboss6/setup/appserver/jboss6 $SILVERPEAS_HOME/setup/appserver
+test $? -eq 0 && cp -r silverpeas-${SILVERPEAS_VERSION}*-jboss6 ${SILVERPEAS_HOME}
 test $? -eq 0 && rm -rf silverpeas-${SILVERPEAS_VERSION}*-jboss6*
 status=$?
-test $status -eq 0 || exit $status
+test ${status} -eq 0 || exit ${status}
+echo "println 'Nothing to do'" > ${SILVERPEAS_HOME}/bin/scripts/configureSpnego.groovy
 
-cd "$SILVERPEAS_HOME/bin"
-mvn clean install $offline
-cp $SCRIPTS_HOME/config.properties $SILVERPEAS_HOME/setup/settings/.
-test $? -eq 0 && $SCRIPTS_HOME/appBuilder.sh
-test $? -eq 0 && $SCRIPTS_HOME/SilverpeasSettings.sh
-test $? -eq 0 && $SCRIPTS_HOME/start_db.sh &
-test $? -eq 0 && $SCRIPTS_HOME/dbBuilder.sh
-test $? -eq 0 && $SCRIPTS_HOME/stop_db.sh &
+echo "  -> copy Silverpeas binaries and settings..."
+cp ${SETTINGS}/config.properties ${SILVERPEAS_HOME}/setup/settings/
+cp ${SETTINGS}/CustomerSettings.xml ${SETTINGS}/config.properties ${WINDOWS_SETTINGS}/
+cp ${SETTINGS}/CustomerSettings.xml ${SETTINGS}/config.properties ${UNIX_SETTINGS}/
+
+echo "  -> build Silverpeas..."
+cd "${SILVERPEAS_HOME}/bin"
+mvn clean install ${offline}
+test $? -eq 0 && ${SCRIPTS_HOME}/appBuilder.sh
+test $? -eq 0 && ${SCRIPTS_HOME}/SilverpeasSettings.sh
+test $? -eq 0 && ${SCRIPTS_HOME}/start_db.sh &
+test $? -eq 0 && ${SCRIPTS_HOME}/dbBuilder.sh
+test $? -eq 0 && ${SCRIPTS_HOME}/stop_db.sh &
 exit $?
